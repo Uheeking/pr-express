1d7f7c92ab3d9dc343e3f4898793a64a0d8d5e39
유희왕 <90121929+Uheeking@users.noreply.github.com>
2023-05-16 12:07:44 +0900

    FIX : 상태 코드 200과 500 분리하기

--- app.js
+++ app.js
@@ -6,37 +6,46 @@ const bodyParser = require("body-parser");
 const { WebClient } = require("@slack/web-api");
 require("dotenv").config();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cors());
-
 const API_TOKEN = process.env.API_TOKEN;
 const web = new WebClient(API_TOKEN);
 const conversationId = process.env.CONVERSATIONID;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
 app.post("/slack", (req, res) => {
   (async () => {
-    const result = await web.chat.postMessage({
-      text:
-        "*이름 :* " +
-        req.body.name +
-        "\n*휴대폰 번호 :* " +
-        req.body.phone +
-        "\n*이메일 :* " +
-        req.body.email +
-        "\n*전할 내용 :* " +
-        req.body.text,
-      channel: conversationId,
-    });
+    try {
+      const result = await web.chat.postMessage({
+        text:
+          "*이름 :* " +
+          req.body.name +
+          "\n*휴대폰 번호 :* " +
+          req.body.phone +
+          "\n*이메일 :* " +
+          req.body.email +
+          "\n*전할 내용 :* " +
+          req.body.text,
+        channel: conversationId,
+      });
 
-    console.log(
-      `Successfully send message ${result} in conversation ${conversationId}`
-    );
-    res.status(200).send({ code: 200, message: "메시지가 성공적으로 전송되었습니다. " });
-    res.status(404).send({ code: 404, message: "메시지 전송이 실패되었습니다. " });
+      console.log(
+        `Successfully send message ${result} in conversation ${conversationId}`
+      );
+      res.status(200).send({
+        code: 200,
+        message: "메시지가 성공적으로 전송되었습니다. ",
+      });
+    } catch (error) {
+      console.error(`Error sending message: ${error}`);
+      res.status(500).send({
+        code: 500,
+        message: "메시지 전송이 실패되었습니다. ",
+      });
+    }
   })();
 });
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
